091d4f6d78b4c0f364842568402f8d1353143db8
Aftertnw <64160049@go.buu.ac.th>
2024-06-17 18:35:57 +0700

    fix merge

--- src/components/headers/MainHeader.vue
+++ src/components/headers/MainHeader.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-import { useUserStore } from "@/stores/user.store";
-const userStore = useUserStore();
-</script>
 <template>
   <v-app-bar scroll-behavior="elevate" color="#3051AC">
     <v-icon style="margin-left: 1%" size="40">mdi-view-grid</v-icon>
-<<<<<<< HEAD
-    <v-app-bar-title>ระบบเช็คชื่อเข้าเรียน</v-app-bar-title>
-    <v-app-bar-title style="text-align: right; margin-right: 2%"
-      >{{userStore.currentUser?.firstName}}  {{ userStore.currentUser?.lastName }}</v-app-bar-title
-=======
     <v-app-bar-title>ระบบจัดการการเข้าเรียนของนิสิต</v-app-bar-title>
     <v-app-bar-title
       style="text-align: right; margin-right: 2%"
@@ -28,7 +19,6 @@ const userStore = useUserStore();
       {{
         userStore.currentUser?.firstName + " " + userStore.currentUser?.lastName
       }}</v-app-bar-title
->>>>>>> 42877e21a02e95af80717b74c7645fa7e8c74664
     >
   </v-app-bar>
 </template>
